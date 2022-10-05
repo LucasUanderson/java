e1327dd8fa8b3b8a43bc27bcb6da38569c96a91e
Lucas Uanderson <104622435+LucasUanderson@users.noreply.github.com>
2022-10-05 10:27:49 -0300

    Initial commit

---
+++ aulas-java/if-else.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int nota = 55;
+        int media= 60;
+
+        if(nota>=media) {
+            System.out.println("Você passou");
+        }else{
+            System.out.println("Você não passou");
+        }
+    }
+    }
